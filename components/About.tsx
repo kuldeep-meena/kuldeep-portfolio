7c34db78d811195d684d15213ff8e305ceb15ca8
kuldeep-meena <kuldeepmeena567@gmail.com>
2024-01-26 11:05:57 +0530

    Intro

--- components/About.tsx
+++ components/About.tsx
@@ -19,13 +19,13 @@ export default function About() {
       <SectionHeading>About Me</SectionHeading>
 
       <p className="mb-3">
-        Upon completing my <span className="font-medium">Electronics Engineering</span>, degree, I opted to follow my enthusiasm for programming. I embarked on a journey of acquiring knowledge in full-stack web development through YouTube tutorials and documentation. The facet of programming that resonates with me the most is the art of troubleshooting and finding resolutions. The gratification derived from unraveling complex problems is truly exhilarating. In my toolkit, I rely on {" "}        <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
-        </span> as the foundational technologies. Additionally, I possess familiarity with TypeScript and Prisma. I maintain a perpetual eagerness to explore novel technological advancements.
+        Upon completing my <span className="font-medium">Mechanical Engineering</span>, degree, I opted to follow my enthusiasm for programming. I embarked on a journey of acquiring knowledge in FrontEnd web development through YouTube tutorials and documentation. The facet of programming that resonates with me the most is the art of troubleshooting and finding resolutions. The gratification derived from unraveling complex problems is truly exhilarating. In my toolkit, I rely on {" "}        <span className="font-medium">
+          JavaScript,React, Next.js,Tailwind
+        </span> as the foundational technologies. Additionally, I possess strong expertise in data structures and algorithms, which enables me to effectively tackle complex problems and devise efficient solutions.  My experience includes participating in coding competitions where I've consistently demonstrated my problem-solving abilities.I maintain a perpetual eagerness to explore novel technological advancements.
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, you'll find me immersed in anime, badminton matches, and PUBG conquests. <span className="font-medium">Learning is my ongoing pleasure</span>, currently focused on delving into the intricate world of backend architecture and the patterns that guide system design. And would you believe it, I'm even mastering the art of playing the guitar.
+        <span className="italic">When I'm not coding</span>, you'll find me immersed in Watching Movies/WebSeries, travelling, and PUBG conquests. <span className="font-medium">Learning is my ongoing pleasure</span>, currently focused on delving into the intricate world of backend architecture and the patterns that guide system design. And would you believe it, I'm even mastering the art of playing the guitar.
       </p>
     </motion.section >
   );
